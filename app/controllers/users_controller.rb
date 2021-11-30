029356d8a706c49eb20f63bd4f73a878b915ea3b
gauti-tag <gautier.tiehoule@gmail.com>
2021-11-30 18:14:02 +0000

    Add javaScript response to friend search form submission

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,29 @@ class UsersController < ApplicationController
   end
   
   def search 
-    render json: params[:friend]
+    #render json: params[:friend]
+    if params[:friend].present?
+      
+      @friend = params[:friend]
+      
+      if @friend
+         respond_to do |format|
+           format.js { render partial: 'users/friend_result' }
+         end
+       else
+         respond_to do |format|
+           flash.now[:alert] = "Can not find user"
+           format.js {render partial: 'users/friend'}
+         end
+      end
+      
+    else 
+      
+      respond_to do |format|
+        flash.now[:alert] = "Please enter a symbol to search"
+        format.js {render partial: 'users/friend_result'}
+      end
+      
+    end
   end
 end
